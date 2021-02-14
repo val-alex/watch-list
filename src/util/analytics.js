ea1b010b5b16addbc70fde68d554d2386eaf1e01
Valentin Alexeev <valentin.m.alexeev@gmail.com>
2021-02-14 23:00:52 +0000

    initial commit

---
+++ src/util/analytics.js
@@ -0,0 +1,25 @@
+import Analytics from "analytics";
+import simpleAnalyticsPlugin from "@analytics/simple-analytics";
+import Router from "next/router";
+
+// Initialize analytics and plugins
+// Documentation: https://getanalytics.io
+const analytics = Analytics({
+  debug: process.env.NODE_ENV !== "production",
+  plugins: [
+    // Instructions: https://divjoy.com/docs/simple-analytics
+    simpleAnalyticsPlugin(),
+  ],
+});
+
+// Track initial pageview
+if (typeof window !== "undefined") {
+  analytics.page();
+}
+
+// Track pageview on route change
+Router.events.on("routeChangeComplete", (url) => {
+  analytics.page();
+});
+
+export default analytics;
